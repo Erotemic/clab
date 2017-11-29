b831ad619fb441069aa40c44e7cf023a1edd263f
joncrall <crallj@rpi.edu>
2017-11-28 23:35:47 -0500

    working on dense unet

--- clab/live/unet3.py
+++ clab/live/unet3.py
@@ -1,17 +1,75 @@
+import torch
+from clab.torch.models import unet
+import torchvision  # NOQA
 import torch.nn as nn
 import functools
 import math
-import torch
+import torch  # NOQA
 import torch.nn.functional as F
 from clab.torch import nninit
 from clab.torch.models.output_shape_for import OutputShapeFor
 
-__all__ = ['UNet']
+__all__ = ['DenseUNet']
 
 
-class UNetConv2(nn.Module):
+def default_nonlinearity():
+    # nonlinearity = functools.partial(nn.ReLU, inplace=False)
+    return nn.LeakyReLU(inplace=True)
+
+
+class _DenseLayer(nn.Sequential):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate=0):
+        super(_DenseLayer, self).__init__()
+        self.bn_size = bn_size
+        self.growth_rate = growth_rate
+        self.n_feat_internal = bn_size * growth_rate
+        self.n_feat_out = num_input_features + growth_rate
+
+        self.add_module('norm.1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('noli.1', nn.ReLU(inplace=True)),
+        self.add_module('conv.1', nn.Conv2d(num_input_features, bn_size *
+                        growth_rate, kernel_size=1, stride=1, bias=False)),
+        self.add_module('norm.2', nn.BatchNorm2d(self.n_feat_internal)),
+        self.add_module('noli.2', nn.ReLU(inplace=True)),
+        self.add_module('conv.2', nn.Conv2d(self.n_feat_internal, growth_rate,
+                        kernel_size=3, stride=1, padding=1, bias=False)),
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        new_features = super(_DenseLayer, self).forward(x)
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+        return torch.cat([x, new_features], 1)
+
+    def output_shape_for(self, input_shape):
+        N1, C1, W1, H1 = input_shape
+        output_shape = (N1, self.n_feat_out, W1, H1)
+        return output_shape
+
+
+class _DenseBlock(nn.Sequential):
+    def __init__(self, num_layers, num_input_features, bn_size=4, growth_rate=32, drop_rate=0):
+        super(_DenseBlock, self).__init__()
+        self.num_layers = num_layers
+        for i in range(num_layers):
+            layer = _DenseLayer(num_input_features + i * growth_rate, growth_rate, bn_size, drop_rate)
+            self.add_module('denselayer%d' % (i + 1), layer)
+        self.n_feat_out = layer.n_feat_out
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('noli', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+
+
+class DenseUNetConv(nn.Module):
     def __init__(self, in_size, out_size, is_batchnorm, nonlinearity='relu'):
-        super(UNetConv2, self).__init__()
+        super(DenseUNetConv, self).__init__()
 
         if nonlinearity == 'relu':
             nonlinearity = functools.partial(nn.ReLU, inplace=False)
@@ -41,35 +99,6 @@ class UNetConv2(nn.Module):
         shape = OutputShapeFor(self.conv2[0])(shape)
         output_shape = shape
         return output_shape
-
-
-# class PadToAgree(nn.Module):
-#     def __init__(self):
-#         super(PadToAgree, self).__init__()
-
-#     def forward(self, inputs1, inputs2):
-#         want_w, want_h = inputs2.size()[-2:]
-#         have_w, have_h = inputs1.size()[-2:]
-
-#         half_offw = (want_w - have_w) / 2
-#         half_offh = (want_h - have_h) / 2
-#         # padding = 2 * [offw // 2, offh // 2]
-
-#         padding = [
-#             # Padding starts from the final dimension and then move backwards.
-#             math.floor(half_offh),
-#             math.ceil(half_offh),
-#             math.floor(half_offw),
-#             math.ceil(half_offw),
-#         ]
-#         outputs1 = F.pad(inputs1, padding)
-#         return outputs1
-
-#     def output_shape_for(self, input_shape1, input_shape2):
-#         N1, C1, W1, H1 = input_shape1
-#         N2, C2, W2, H2 = input_shape2
-#         output_shape = (N1, C1, W2, H2)
-#         return output_shape
 
 
 class PadToAgree(nn.Module):
@@ -116,21 +145,39 @@ class PadToAgree(nn.Module):
         return output_shape
 
 
-class UNetUp(nn.Module):
+class DenseUNetUp(nn.Module):
     """
+        Ignore:
+            in_size = 64
+            out_size = 32
+            down = _DenseBlock(3, 64, 4)
+            self = DenseUNetUp(in_size1=64, in_size2=160)
+            num_layers = 2
     """
-    def __init__(self, in_size, out_size, is_deconv=True, nonlinearity='relu'):
-        super(UNetUp, self).__init__()
+    def __init__(self, in_size1, in_size2, compress=.5, num_layers=2,
+                 growth_rate=4, is_deconv=True):
+        super(DenseUNetUp, self).__init__()
+        out_size2 = int(compress * in_size2)
         if is_deconv:
-            self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
+            self.up = nn.ConvTranspose2d(in_size2, out_size2, kernel_size=2, stride=2)
         else:
             self.up = nn.UpsamplingBilinear2d(scale_factor=2)
         self.pad = PadToAgree()
-        self.conv = UNetConv2(in_size, out_size, is_batchnorm=False,
-                              nonlinearity=nonlinearity)
+        in_size = in_size1 + out_size2
+        self.conv = _DenseBlock(num_layers, in_size, growth_rate=growth_rate,
+                                bn_size=4)
+        bneck_size = int(in_size1 * compress)
+        self.bottleneck = nn.Conv2d(self.conv.n_feat_out, bneck_size,
+                                    kernel_size=1, stride=1, bias=False)
+        self.n_feat_out = bneck_size
+
+        # self.conv = UNetConv2(in_size, num_layers, is_batchnorm=False,
+        #                       nonlinearity=nonlinearity)
 
     def output_shape_for(self, input1_shape, input2_shape):
         """
+
+        Example:
             >>> self = UNetUp(256, 128)
             >>> input1_shape = [4, 128, 24, 24]
             >>> input2_shape = [4, 256, 8, 8]
@@ -144,7 +191,8 @@ class UNetUp(nn.Module):
         output2_shape = OutputShapeFor(self.up)(input2_shape)
         output1_shape = OutputShapeFor(self.pad)(input1_shape, output2_shape)
         cat_shape     = OutputShapeFor(torch.cat)([output1_shape, output2_shape], 1)
-        output_shape  = OutputShapeFor(self.conv)(cat_shape)
+        conv_shape  = OutputShapeFor(self.conv)(cat_shape)
+        output_shape  = OutputShapeFor(self.bottleneck)(conv_shape)
         return output_shape
 
     def forward(self, inputs1, inputs2):
@@ -164,10 +212,10 @@ class UNetUp(nn.Module):
         outputs2 = self.up(inputs2)
         outputs1 = self.pad(inputs1, outputs2)
         outputs_cat = torch.cat([outputs1, outputs2], 1)
-        return self.conv(outputs_cat)
+        return self.bottleneck(self.conv(outputs_cat))
 
 
-class UNet(nn.Module):
+class DenseUNet(nn.Module):
     """
     Note input shapes should be a power of 2.
 
@@ -191,8 +239,10 @@ class UNet(nn.Module):
     Example:
         >>> from clab.torch.models.unet import *  # NOQA
         >>> from torch.autograd import Variable
+        >>> input_shape = (4, 6, 480, 360)
         >>> B, C, W, H = (4, 5, 480, 360)
         >>> n_classes = 11
+        >>> in_channels = 3
         >>> inputs = Variable(torch.rand(B, C, W, H))
         >>> labels = Variable((torch.rand(B, W, H) * n_classes).long())
         >>> self = UNet(in_channels=C, n_classes=n_classes)
@@ -201,38 +251,80 @@ class UNet(nn.Module):
         >>> print('outputs.size() = {!r}'.format(outputs.size()))
         >>> print(np.array(inputs.size()) - np.array(outputs.size()))
     """
-    def __init__(self, feature_scale=4, n_classes=21, is_deconv=True,
-                 in_channels=3, is_batchnorm=True, nonlinearity='relu'):
-        super(UNet, self).__init__()
-        self.is_deconv = is_deconv
+    def __init__(self, n_classes=21, is_deconv=True, in_channels=3):
+        super(DenseUNet, self).__init__()
         self.in_channels = in_channels
-        self.is_batchnorm = is_batchnorm
-        self.feature_scale = feature_scale
-        self.nonlinearity = nonlinearity
 
-        filters = [64, 128, 256, 512, 1024]
-        filters = [int(x // self.feature_scale) for x in filters]
+        import ubelt as ub
+
+        n_feat0 = 64
+        from torch import nn
+        features = nn.Sequential(ub.odict([
+            ('conv0', nn.Conv2d(in_channels, n_feat0, kernel_size=7, stride=1, padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(n_feat0)),
+            ('noli0', default_nonlinearity()),
+            # ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        block_config = [3, 4, 4, 4, 4]
+        bn_size = 4
+        compress = .5
+        growth_rate = 32
+
+        n_feat = n_feat0
 
         # downsampling
-        self.conv1 = UNetConv2(self.in_channels, filters[0], self.is_batchnorm, self.nonlinearity)
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+        down = []
+        densekw = dict(bn_size=bn_size, growth_rate=growth_rate)
 
-        self.conv2 = UNetConv2(filters[0], filters[1], self.is_batchnorm, self.nonlinearity)
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+        for i, num_layers in enumerate(block_config[0:-1]):
+            down.append(('denseblock%d' % (i + 1), _DenseBlock(num_layers=num_layers, num_input_features=n_feat, **densekw)))
+            n_feat = n_feat + num_layers * growth_rate
+            n_feat_compress = int(n_feat * compress)
+            down.append(('transition%d' % (i + 1), _Transition(num_input_features=n_feat, num_output_features=n_feat_compress)))
+            n_feat = n_feat_compress
 
-        self.conv3 = UNetConv2(filters[1], filters[2], self.is_batchnorm, self.nonlinearity)
-        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+        down_net = nn.Sequential(ub.odict(down))
 
-        self.conv4 = UNetConv2(filters[2], filters[3], self.is_batchnorm, self.nonlinearity)
-        self.maxpool4 = nn.MaxPool2d(kernel_size=2)
+        num_layers = block_config[-1]
+        center5 = _DenseBlock(num_layers=num_layers, num_input_features=n_feat, **densekw)
+        n_feat = n_feat + num_layers * growth_rate
 
-        self.center = UNetConv2(filters[3], filters[4], self.is_batchnorm, self.nonlinearity)
+        center5.n_feat_out
+
+        up_concat4 = DenseUNetUp(down_net.denseblock4.n_feat_out, center5.n_feat_out)
+        up_concat3 = DenseUNetUp(down_net.denseblock3.n_feat_out, up_concat4.n_feat_out)
+        up_concat2 = DenseUNetUp(down_net.denseblock2.n_feat_out, up_concat3.n_feat_out)
+        up_concat1 = DenseUNetUp(down_net.denseblock1.n_feat_out, up_concat2.n_feat_out)
+
+        self.up_concat4 = up_concat4
+        self.up_concat3 = up_concat3
+        self.up_concat2 = up_concat2
+        self.up_concat1 = up_concat1
+
+        feat_shape = OutputShapeFor(features)(input_shape)
+        dense_shape = OutputShapeFor(down_net)(feat_shape)
+
+        # downsampling
+        # self.conv1 = UNetConv2(self.in_channels, filters[0], self.is_batchnorm)
+        # self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        # self.conv2 = UNetConv2(filters[0], filters[1], self.is_batchnorm, self.nonlinearity)
+        # self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        # self.conv3 = UNetConv2(filters[1], filters[2], self.is_batchnorm, self.nonlinearity)
+        # self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+
+        # self.conv4 = UNetConv2(filters[2], filters[3], self.is_batchnorm, self.nonlinearity)
+        # self.maxpool4 = nn.MaxPool2d(kernel_size=2)
+
+        # self.center = UNetConv2(filters[3], filters[4], self.is_batchnorm, self.nonlinearity)
 
         # upsampling
-        self.up_concat4 = UNetUp(filters[4], filters[3], self.is_deconv, self.nonlinearity)
-        self.up_concat3 = UNetUp(filters[3], filters[2], self.is_deconv, self.nonlinearity)
-        self.up_concat2 = UNetUp(filters[2], filters[1], self.is_deconv, self.nonlinearity)
-        self.up_concat1 = UNetUp(filters[1], filters[0], self.is_deconv, self.nonlinearity)
+        # self.up_concat4 = DenseUNetUp(filters[4], filters[3], self.is_deconv)
+        # self.up_concat3 = DenseUNetUp(filters[3], filters[2], self.is_deconv)
+        # self.up_concat2 = DenseUNetUp(filters[2], filters[1], self.is_deconv)
+        # self.up_concat1 = DenseUNetUp(filters[1], filters[0], self.is_deconv)
 
         # final conv (without any concat)
         self.final = nn.Conv2d(filters[0], n_classes, 1)
@@ -564,3 +656,95 @@ class UNet(nn.Module):
             else:
                 nninit.he_normal(self_state[key])
         model.load_state_dict(self_state)
+
+
+class UNet2(unet.UNet):
+    """
+    Note input shapes should be a power of 2.
+
+    In this case there will be a ~188 pixel difference between input and output
+    dims, so the input should be mirrored with
+
+    Example:
+        >>> from clab.live.unet2 import *  # NOQA
+        >>> from torch.autograd import Variable
+        >>> B, C, W, H = (4, 3, 256, 256)
+        >>> B, C, W, H = (4, 3, 572, 572)
+        >>> n_classes = 11
+        >>> inputs = Variable(torch.rand(B, C, W, H))
+        >>> labels = Variable((torch.rand(B, W, H) * n_classes).long())
+        >>> self = UNet2(in_channels=C, n_classes=n_classes)
+        >>> outputs = self.forward(inputs)
+        >>> print('inputs.shape = {!r}'.format(inputs.shape))
+        >>> print('outputs.shape = {!r}'.format(outputs.shape))
+        >>> print(np.array(inputs.shape) - np.array(outputs.shape))
+
+    Example:
+        >>> from clab.torch.models.unet import *  # NOQA
+        >>> from torch.autograd import Variable
+        >>> B, C, W, H = (4, 5, 480, 360)
+        >>> n_classes = 11
+        >>> inputs = Variable(torch.rand(B, C, W, H))
+        >>> labels = Variable((torch.rand(B, W, H) * n_classes).long())
+        >>> self = UNet(in_channels=C, n_classes=n_classes)
+        >>> outputs = self.forward(inputs)
+        >>> print('inputs.shape = {!r}'.format(inputs.shape))
+        >>> print('outputs.shape = {!r}'.format(outputs.shape))
+        >>> print(np.array(inputs.shape) - np.array(outputs.shape))
+    """
+    def __init__(self, n_alt_classes=2, **kwargs):
+        super(UNet2, self).__init__(**kwargs)
+
+        self.final
+        self.final2 = torch.nn.Conv2d(self.final.in_channels, n_alt_classes, 1)
+
+    def raw_forward2(self, inputs):
+        conv1 = self.conv1(inputs)
+        maxpool1 = self.maxpool1(conv1)
+
+        conv2 = self.conv2(maxpool1)
+        maxpool2 = self.maxpool2(conv2)
+
+        conv3 = self.conv3(maxpool2)
+        maxpool3 = self.maxpool3(conv3)
+
+        conv4 = self.conv4(maxpool3)
+        maxpool4 = self.maxpool4(conv4)
+
+        center = self.center(maxpool4)
+
+        up4 = self.up_concat4(conv4, center)
+        up3 = self.up_concat3(conv3, up4)
+        up2 = self.up_concat2(conv2, up3)
+        up1 = self.up_concat1(conv1, up2)
+
+        final = self.final(up1)
+        final2 = self.final2(up1)
+        return final, final2
+
+    def forward(self, inputs):
+        """
+            >>> from clab.torch.models.unet import *  # NOQA
+            >>> import torch
+            >>> from torch.autograd import Variable
+            >>> B, C, W, H = (4, 5, 256, 256)
+            >>> n_classes = 11
+            >>> inputs = Variable(torch.rand(B, C, W, H))
+            >>> labels = Variable((torch.rand(B, W, H) * n_classes).long())
+            >>> self = UNet(in_channels=C, n_classes=n_classes)
+            >>> outputs = self.forward(inputs)
+        """
+        # Is there a way to miror so that we have enough input pixels?
+        # so we can crop off extras after?
+        if isinstance(inputs, (list, tuple)):
+            assert len(inputs) == 1, 'unet2 only takes single branch input'
+            inputs = inputs[0]
+
+        mirrored = inputs
+        mirrored, crop_wh = self.prepad(inputs)
+
+        final1, final2 = self.raw_forward2(mirrored)
+
+        cropped1 = self.postcrop(final1, crop_wh)
+        cropped2 = self.postcrop(final2, crop_wh)
+        return cropped1, cropped2
