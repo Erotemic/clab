bcfe45a3f8c7e72f702a20a16aab205693a2a717
joncrall <crallj@rpi.edu>
2018-03-18 14:49:30 -0400

    wip

--- examples/yolo.py
+++ examples/yolo.py
@@ -15,10 +15,41 @@ import torch
 import glob
 import ubelt as ub
 import numpy as np
-from torch.utils.data import Dataset
+import torch.utils.data
 
 
-class VOCDataset(Dataset):
+class MultiScaleBatchSampler(torch.utils.data.BatchSampler):
+    def __init__(self, data_source, shuffle=False, batch_size=16,
+                 drop_last=True):
+        if shuffle:
+            self.sampler = torch.utils.data.RandomSampler(data_source)
+        else:
+            self.sampler = torch.utils.data.SequentialSampler(data_source)
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.num_scales = len(self.data_source.multi_scale_inp_size)
+
+    def __iter__(self):
+        batch = []
+        if self.shuffle:
+            scale_index = int(torch.rand(1) * self.num_scales)
+        else:
+            scale_index = 0
+
+        for idx in self.sampler:
+            batch.append((int(idx), scale_index))
+            if len(batch) == self.batch_size:
+                yield batch
+                if self.shuffle:
+                    # choose a new scale index
+                    scale_index = int(torch.rand(1) * self.num_scales)
+                batch = []
+        if len(batch) > 0 and not self.drop_last:
+            yield batch
+
+
+class VOCDataset(torch.utils.data.Dataset):
     """
     Notes:
         mkdir -p ~/data/VOC
@@ -32,19 +63,18 @@ class VOCDataset(Dataset):
         tar xvf VOCdevkit_08-Jun-2007.tar
 
     Example:
-        >>> datadir = ub.truepath('~/data/VOC')
-        >>> self = VOCDataset(datadir)
+        >>> self = VOCDataset()
         >>> for i in range(10):
         ...     a, bc = self[i]
         ...     #print(bc[0].shape)
         ...     print(bc[1].shape)
         ...     print(a.shape)
     """
-    def __init__(self, datadir=None, split='train'):
-        if datadir is None:
-            datadir = ub.truepath('~/data/VOC')
+    def __init__(self, devkit_dpath=None, split='train'):
+        if devkit_dpath is None:
+            devkit_dpath = ub.truepath('~/data/VOC/VOCdevkit')
 
-        data_dpath = join(datadir, 'VOCdevkit', 'VOC2007')
+        data_dpath = join(devkit_dpath, 'VOC2007')
         image_dpath = join(data_dpath, 'JPEGImages')
         annot_dpath = join(data_dpath, 'Annotations')
 
@@ -71,9 +101,6 @@ class VOCDataset(Dataset):
         self.apaths = [join(annot_dpath, '{:06d}.xml'.format(idx))
                        for idx in split_idxs]
 
-        # self.gpaths = sorted(glob.glob(join(image_dpath, '*.jpg')))
-        # self.apaths = sorted(glob.glob(join(annot_dpath, '*.xml')))
-
         self.label_names = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
                             'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
                             'dog', 'horse', 'motorbike', 'person',
@@ -93,17 +120,41 @@ class VOCDataset(Dataset):
         self.num_classes = len(self.label_names)
         self.num_anchors = len(self.anchors)
 
-    def make_loader(self, **kwargs):
+    def make_loader(self, *args, **kwargs):
         """
         We need to do special collation to deal with different numbers of
         bboxes per item.
+
+        Args:
+            batch_size (int, optional): how many samples per batch to load
+                (default: 1).
+            shuffle (bool, optional): set to ``True`` to have the data reshuffled
+                at every epoch (default: False).
+            sampler (Sampler, optional): defines the strategy to draw samples from
+                the dataset. If specified, ``shuffle`` must be False.
+            batch_sampler (Sampler, optional): like sampler, but returns a batch of
+                indices at a time. Mutually exclusive with batch_size, shuffle,
+                sampler, and drop_last.
+            num_workers (int, optional): how many subprocesses to use for data
+                loading. 0 means that the data will be loaded in the main process.
+                (default: 0)
+            pin_memory (bool, optional): If ``True``, the data loader will copy tensors
+                into CUDA pinned memory before returning them.
+            drop_last (bool, optional): set to ``True`` to drop the last incomplete batch,
+                if the dataset size is not divisible by the batch size. If ``False`` and
+                the size of dataset is not divisible by the batch size, then the last batch
+                will be smaller. (default: False)
+            timeout (numeric, optional): if positive, the timeout value for collecting a batch
+                from workers. Should always be non-negative. (default: 0)
+            worker_init_fn (callable, optional): If not None, this will be called on each
+                worker subprocess with the worker id (an int in ``[0, num_workers - 1]``) as
+                input, after seeding and before data loading. (default: None)
 
         References:
             https://github.com/pytorch/pytorch/issues/1512
 
         Example:
-            >>> datadir = ub.truepath('~/data/VOC')
-            >>> self = VOCDataset(datadir)
+            >>> self = VOCDataset()
             >>> #inbatch = [self[i] for i in range(10)]
             >>> loader = self.make_loader(batch_size=10)
             >>> batch = next(iter(loader))
@@ -114,7 +165,7 @@ class VOCDataset(Dataset):
         """
         from torch.utils.data import DataLoader
         from torch.utils.data.dataloader import default_collate
-        def collate_fn(inbatch):
+        def custom_collate_fn(inbatch):
             # we know the order of data in __getitem__ so we can choose not to
             # stack the variable length bboxes and labels
             inimgs, inlabels = list(map(list, zip(*inbatch)))
@@ -122,7 +173,8 @@ class VOCDataset(Dataset):
             labels = list(map(list, zip(*inlabels)))
             batch = imgs, labels
             return batch
-        loader = DataLoader(self, collate_fn=collate_fn, **kwargs)
+        kwargs['collate_fn'] = custom_collate_fn
+        loader = DataLoader(self, *args, **kwargs)
         return loader
 
     def __len__(self):
@@ -137,7 +189,10 @@ class VOCDataset(Dataset):
             bbox is in x1,y1,x2,y2 format
         """
         # size_index = randint(0, len(cfg.multi_scale_inp_size) - 1)
-        size_index = 0
+        if isinstance(index, tuple):
+            index, size_index = index
+        else:
+            size_index = 0
         hwc, boxes, gt_classes = self._load_item(index, size_index)
 
         # TODO: augmentation
@@ -217,29 +272,132 @@ class VOCDataset(Dataset):
         return annots
 
 
+def make_loaders(datasets, train_batch_size=16, vali_batch_size=1, workers=0):
+    loaders = {}
+    for key, dset in datasets.items():
+        batch_size = train_batch_size if key == 'train' else vali_batch_size
+        # use custom sampler that does multiscale training
+        batch_sampler = MultiScaleBatchSampler(
+            dset, batch_size=batch_size, shuffle=(key == 'train')
+        )
+        loader = dset.make_loader(batch_sampler=batch_sampler,
+                                  num_workers=workers)
+        loaders[key] = loader
+    return loaders
+
+
 def train():
     from clab.models.yolo2 import darknet
+
+    class cfg(object):
+        start_step = 0
+        lr_decay = 1. / 10
+
+        workers = 0
+
+        max_epoch = 160
+
+        weight_decay = 0.0005
+        momentum = 0.9
+        init_learning_rate = 1e-3
+
+        # for training yolo2
+        # object_scale = 5.
+        # noobject_scale = 1.
+        # class_scale = 1.
+        # coord_scale = 1.
+        # iou_thresh = 0.6
+
+        # dataset
+        vali_batch_size = 1
+        train_batch_size = 16
+
+        lr_mapping = {
+            0: init_learning_rate * lr_decay ** 0,
+            60: init_learning_rate * lr_decay ** 1,
+            90: init_learning_rate * lr_decay ** 2,
+        }
+
+        workdir = ub.truepath('~/work/VOC2007')
+        devkit_dpath = ub.truepath('~/data/VOC/VOCdevkit')
+
     datasets = {
-        'train': VOCDataset(split='train'),
-        'vali': VOCDataset(split='val'),
-    }
-    batch_size = 4
-    workers = 0
-
-    # TODO: custom sampler that does multiscale training
-    loaders = {
-        key: dset.make_loader(batch_size=batch_size, num_workers=workers)
-        for key, dset in datasets.items()
+        'train': VOCDataset(cfg.devkit_dpath, split='train'),
+        'vali': VOCDataset(cfg.devkit_dpath, split='val'),
     }
 
-    dset = datasets['train']
-    model = (darknet.Darknet19,
-             {'num_classes': dset.num_classes,
-              'anchors': dset.anchors})
-    criterion = (darknet.DarknetLoss, {'anchors': dset.anchors})
+    loaders = make_loaders(datasets,
+                           train_batch_size=cfg.train_batch_size,
+                           vali_batch_size=cfg.vali_batch_size,
+                           workers=cfg.workers)
 
     from clab import hyperparams
+    from clab import xpu_device
+    from clab import fit_harness
+    from clab import monitor
+    from clab.lr_scheduler import ListedLR
+
     hyper = hyperparams.HyperParams(
-        model=model,
-        criterion=criterion,
+
+        model=(darknet.Darknet19, {
+            'num_classes': datasets['train'].num_classes,
+            'anchors': datasets['train'].anchors
+        }),
+
+        criterion=(darknet.DarknetLoss, {
+            'anchors': datasets['train'].anchors
+        }),
+
+        optimizer=(torch.optim.SGD, dict(
+            lr=cfg.init_learning_rate,
+            momentum=cfg.momentum,
+            weight_decay=cfg.weight_decay
+        )),
+
+        scheduler=(ListedLR, dict(
+            mapping=cfg.lr_mapping
+        )),
+
+        other={
+            'train_batch_size': cfg.train_batch_size,
+        },
+        centering=None,
+        # centering=datasets['train'].centering,
+        # augmenter=datasets['train'].augmenter,
     )
+
+    xpu = xpu_device.XPU.cast('auto')
+    harn = fit_harness.FitHarness(
+        hyper=hyper, datasets=datasets, xpu=xpu,
+        loaders=loaders,
+    )
+    harn.monitor = monitor.Monitor(min_keys=['loss'],
+                                   # max_keys=['global_acc', 'class_acc'],
+                                   patience=40)
+
+    @harn.set_batch_runner
+    def batch_runner(harn, inputs, labels):
+        """
+        Custom function to compute the output of a batch and its loss.
+        """
+        output = harn.model(*inputs)
+        label = labels[0]
+        loss = harn.criterion(output, label)
+        outputs = [output]
+        return outputs, loss
+
+    # @harn.add_metric_hook
+    # def custom_metrics(harn, outputs, labels):
+    #     label = labels[0]
+    #     output = outputs[0]
+
+    #     y_pred = output.data.max(dim=1)[1].cpu().numpy()
+    #     y_true = label.data.cpu().numpy()
+
+    #     metrics_dict = ub.odict()
+    #     metrics_dict['global_acc'] = global_acc
+    #     metrics_dict['class_acc'] = class_accuracy
+    #     return metrics_dict
+
+    harn.setup_dpath(ub.ensuredir(cfg.workdir))
+    harn.run()
