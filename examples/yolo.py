55e2b7e962ead86d43eb16dc359f210426d8972d
Jon Crall <jon.crall@kitware.com>
2018-03-19 16:36:56 -0400

    wip

--- examples/yolo.py
+++ examples/yolo.py
@@ -261,7 +261,7 @@ def grab_darknet19_initial_weights():
 class cfg(object):
     n_cpus = psutil.cpu_count(logical=True)
 
-    workers = n_cpus / 2
+    workers = int(n_cpus / 2)
 
     max_epoch = 160
 
