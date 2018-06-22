cc83e95af29d9d719b9954e5274bd84dd9acbd44
joncrall <jon.crall@kitware.com>
2018-06-22 19:44:49 -0400

    wip

--- clab/util/__init__.py
+++ clab/util/__init__.py
@@ -1,5 +1,5 @@
 """
-mkinit clab.util
+mkinit ~/code/clab/clab/util
 """
 # flake8: noqa
 from __future__ import absolute_import, division, print_function, unicode_literals
@@ -26,6 +26,7 @@ else:
     from clab.util import util_affine
     from clab.util import util_alg
     from clab.util import util_averages
+
     from clab.util.colorutil import (colorbar_image, convert_hex_to_255,
                                      lookup_bgr255, make_distinct_bgr01_colors,
                                      make_distinct_bgr255_colors, make_heatmask,)
@@ -89,4 +90,53 @@ else:
     from clab.util.util_averages import (CumMovingAve, ExpMovingAve,
                                          InternalRunningStats, MovingAve,
                                          RunningStats, WindowedMovingAve, absdev,)
+
+    __all__ = ['Boxes', 'CV2_INTERPOLATION_TYPES', 'Color', 'CumMovingAve',
+               'ExpMovingAve', 'IS_PROFILING', 'InternalRunningStats',
+               'JSONEncoder', 'KernprofParser', 'MovingAve',
+               'NumpyAwareJSONEncoder', 'NumpyEncoder', 'PauseTQDM', 'PlotNums',
+               'PriorityQueue', 'RunningStats', 'SortedQueue', 'TRANSFORM_DTYPE',
+               'WindowedMovingAve', 'absdev', 'adjust_gamma', 'adjust_subplots',
+               'affine_around_mat3x3', 'affine_mat3x3', 'align_paths',
+               'apply_grouping', 'argsubmax', 'argsubmaxima', 'atleast_3channels',
+               'atleast_nd', 'axes_extent', 'cc_locs', 'check_aligned',
+               'clean_tensorboard_protobufs', 'colorbar', 'colorbar_image',
+               'colorutil', 'compact_idstr', 'convert_colorspace',
+               'convert_hex_to_255', 'copy_figure_to_clipboard', 'coverage_kpts',
+               'deterministic_shuffle', 'dict_intersection', 'distinct_colors',
+               'distinct_markers', 'draw_border', 'draw_boxes',
+               'draw_line_segments', 'dump_global_profile_report',
+               'dump_nx_ondisk', 'dumpsafe', 'dynamic_profile',
+               'ensure_alpha_channel', 'ensure_float01', 'ensure_fnum',
+               'ensure_grayscale', 'ensure_rng', 'extract_axes_extents', 'figure',
+               'find_parent_class', 'find_pattern_above_row',
+               'find_pyclass_above_row', 'find_unused_gpu', 'fnameutil',
+               'gaussian_patch', 'get_num_channels', 'get_stack_frame', 'gpu_info',
+               'gpu_util', 'grab_test_image', 'grab_test_imgpath', 'group_indices',
+               'group_items', 'hash_data', 'hash_file', 'hashutil', 'have_gpu',
+               'image_slices', 'imread', 'imscale', 'imshow', 'imutil', 'imwrite',
+               'isect_flags', 'isiterable', 'iter_reduce_ufunc',
+               'json_numpy_obj_hook', 'jsonutil', 'legend', 'load_image_paths',
+               'logger', 'lookup_bgr255', 'make_agraph',
+               'make_channels_comparable', 'make_distinct_bgr01_colors',
+               'make_distinct_bgr255_colors', 'make_heatmask', 'make_idstr',
+               'make_kpts_coverage_mask', 'make_kpts_heatmask', 'make_short_idstr',
+               'mincost_assignment', 'misc', 'mplutil', 'multi_plot', 'next_fnum',
+               'nputil', 'num_gpus', 'nx_delete_None_edge_attr',
+               'nx_delete_node_attr', 'nx_ensure_agraph_color', 'nx_sink_nodes',
+               'nx_source_nodes', 'nxutil', 'overlay_alpha_images',
+               'overlay_colorized', 'pandas_plot_matrix', 'patch_pygraphviz',
+               'priority_queue', 'profile', 'profile_onthefly', 'profiler',
+               'protect_print', 'putMultiLineText', 'qtensure', 'random_indices',
+               'read_arr', 'read_h5arr', 'read_json', 'render_figure_to_image',
+               'reverse_colormap', 'rotation_around_bbox_mat3x3',
+               'rotation_around_mat3x3', 'rotation_mat2x2', 'rotation_mat3x3',
+               'roundrobin', 'run_length_encoding', 'save_parts', 'savefig2',
+               'scale_around_mat3x3', 'scale_boxes', 'scale_mat3x3',
+               'scores_to_cmap', 'scores_to_color', 'set_figtitle', 'shear_mat3x3',
+               'shortest_unique_prefixes', 'shortest_unique_suffixes',
+               'show_if_requested', 'super2', 'transform_around',
+               'translation_mat3x3', 'util_affine', 'util_alg', 'util_averages',
+               'walk_json', 'wide_strides_1d', 'write_arr', 'write_h5arr',
+               'write_json']
     # </AUTOGEN_INIT>
